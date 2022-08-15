767eb9aa07351b41a11171314c23616034168438
Shankha <ribhumaster@gmail.com>
2022-08-15 15:33:50 +0530

    user location fetched

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,11 +1,16 @@
 import {
   launchCameraAsync,
-  useCameraPermissions,
   PermissionStatus,
+  useCameraPermissions,
 } from "expo-image-picker";
-import { Alert, Button, View } from "react-native";
+import { useState } from "react";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -40,15 +45,40 @@ function ImagePicker() {
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    setPickedImage(image.uri);
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 4,
+  },
+});
 
 export default ImagePicker;
---
+++ util/location.js
@@ -0,0 +1,8 @@
+const API_KEY = "iDoNoThAvEmOnEyToAfFoRdGoOgLeMaPs";
+
+export function getMapPreview(lat, lng) {
+  const imagePreviewUrl =
+    "https://risanb.com/code/colorful-google-maps-marker/default-marker.jpg";
+
+  return imagePreviewUrl;
+}
